2ced79045ca2f33f1a64ea7fe1c2a971bfdb780e
spinaprinceps <prajnaspd@gmail.com>
2025-03-06 22:26:27 +0530

    j

--- Client/src/components/dashboard/dashboard.jsx
+++ Client/src/components/dashboard/dashboard.jsx
@@ -47,25 +47,28 @@ const TextEditor = () => {
   };
 
   return (
-    <div style={{ padding: "20px", textAlign: "center", borderBlock: "30px", fontFamily:"nunito", fontWeight:"bold" }}>
-      <h2>Text Processing</h2>
-      <textarea
-        rows="4"
-        cols="50"
-        value={inputText}
-        onChange={(e) => setInputText(e.target.value)}
-        placeholder="Enter text here..."
-        style={{ width: "100%", padding: "10px" }}
-      />
-      <br />
-      <button onClick={processText} disabled={loading}>
-        {loading ? "Processing..." : "Process Text"}
-      </button>
-      <br /><br />
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <h3>Processed Text:</h3>
-      <div style={{ fontFamily: "OpenDyslexic, sans-serif", padding: "10px", border: "1px solid black", backgroundColor:"#E48E7Fff"}}>
-        <p style={{ fontSize: "36px", fontWeight: "bold" }}>{outputText}</p>
+    <div className="h-screen w-full flex bg-[#DDD0C8] p-8">
+      <div className="flex-1 flex flex-col items-center p-4">
+        <label className="text-[#323232] text-4xl font-extrabold font-mono mb-2">TEXT PROCESSING</label>
+        <textarea
+          className="w-full h-[70vh] p-4 bg-[#e2a59b] text-[#323232] rounded-lg focus:outline-none resize-none"
+          placeholder="Enter text here..."
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        <button
+          className="bg-[#323232] text-white px-6 py-3 rounded-lg text-lg hover:bg-gray-700 transition mt-4"
+          onClick={processText}
+          disabled={loading}
+        >
+          {loading ? "Processing..." : "Process Text"}
+        </button>
+
+        {error && <p className="text-red-500 mt-2 p-2">{error}</p>}
+        <h3 className="text-2xl font-extrabold mt-4 p-2">Processed Text:</h3>
+        <div className="mt-4 p-4 bg-[#96C0B2ff] rounded-lg w-full text-center">
+          <p className="text-4xl font-mono p-2" style={{ fontFamily: "OpenDyslexic, sans-serif" }}>{outputText}</p>
+        </div>
       </div>
     </div>
   );
